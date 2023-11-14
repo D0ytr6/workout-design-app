c4a9b66370b5c65c36bdaf554fc42fc1e9fe7b48
Dmytro <vasyuk.dima27@gmail.com>
2023-11-14 10:43:03 +0200

    Add buttons on workout items, fix double opening keyboard

--- app/src/main/java/com/example/samurairoad/ui/activities/WorkoutsListFragment.kt
+++ app/src/main/java/com/example/samurairoad/ui/activities/WorkoutsListFragment.kt
@@ -1,27 +1,28 @@
 package com.example.samurairoad.ui.activities
 
-import android.app.Dialog
+import android.content.Context.*
 import android.os.Bundle
 import android.os.Parcel
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.CompoundButton
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.samurairoad.R
-import com.example.samurairoad.adapters.models.Workout
 import com.example.samurairoad.adapters.ParentWorkoutAdapter
+import com.example.samurairoad.adapters.models.Workout
 import com.example.samurairoad.databinding.FragmentWorkoutsListBinding
 import com.example.samurairoad.dialogs.CreateWorkoutDialog
 import com.example.samurairoad.repository.WorkoutRepository
-import com.example.samurairoad.room.tables.ExerciseTableModel
-import com.example.samurairoad.room.tables.WorkoutTableModel
 
 
 class WorkoutsListFragment : Fragment(), ParentWorkoutAdapter.OnItemWorkoutClickListener{
@@ -34,6 +35,7 @@ class WorkoutsListFragment : Fragment(), ParentWorkoutAdapter.OnItemWorkoutClick
     private val binding get() = _binding!!
 
     private lateinit var viewModel: WorkoutsListViewModel
+    private var workoutList = emptyList<Workout>()
 
     // callback for dialog
     private val listener = object : CreateWorkoutDialog.DialogClickListener{
@@ -68,12 +70,21 @@ class WorkoutsListFragment : Fragment(), ParentWorkoutAdapter.OnItemWorkoutClick
 
         binding.workoutListRv.adapter = adapter
 
+        binding.searchBar.isActivated = true;
+        binding.searchBar.queryHint = "Type your keyword here";
+        binding.searchBar.onActionViewExpanded();
+        binding.searchBar.isIconified = false;
+        binding.searchBar.clearFocus();
+
         viewModel.workoutAdapterList.observe(viewLifecycleOwner){
             Log.d(WorkoutRepository.MyTag, "Fragment get data from observer")
             adapter.workouts = it
+            workoutList = it
         }
 
         binding.fabBtnCreateWorkout.setOnClickListener {
+            binding.searchBar.setQuery("", false);
+            binding.root.requestFocus();
             openCreateWorkoutDialog()
         }
 
@@ -89,14 +100,49 @@ class WorkoutsListFragment : Fragment(), ParentWorkoutAdapter.OnItemWorkoutClick
 
         })
 
+        //TODO fix hide soft keyboard
+//        binding.root.setOnTouchListener(object: View.OnTouchListener{
+//            override fun onTouch(p0: View?, p1: MotionEvent?): Boolean {
+//                binding.searchBar.setQuery("", false);
+//                binding.root.requestFocus();
+//                hideKeyboard()
+//                return true
+//            }
+//
+//        })
+
         return binding.root
     }
+
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         Log.d(WorkoutRepository.LifecycleTag, "onViewCreated")
         super.onViewCreated(view, savedInstanceState)
         viewModel.getAllWorkouts(requireContext())
+
+        binding.searchBar.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                var tempList = mutableListOf<Workout>()
+
+                for(workout in workoutList){
+                    if(workout.title.lowercase().contains(p0!!)){
+                        tempList.add(workout)
+                    }
+                }
+
+                adapter.workouts = tempList
+                return true
+
+            }
+
+        })
+
         //viewModel.getAllExercises(requireContext())
     }
 
@@ -130,6 +176,14 @@ class WorkoutsListFragment : Fragment(), ParentWorkoutAdapter.OnItemWorkoutClick
     override fun onStop() {
         super.onStop()
         Log.d(WorkoutRepository.LifecycleTag, "onStop")
+    }
+
+    private fun hideKeyboard() {
+        val view: View? = requireActivity().currentFocus;
+        val imm = requireActivity().getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        if (view != null) {
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
     }
 
     private fun openCreateWorkoutDialog(){
